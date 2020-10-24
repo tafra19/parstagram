c562ac1ea7d2318bbab90b37f6a349daf416719c
Tazkia Afra <tazkia.afra@gmail.com>
2020-10-24 00:49:09 -0400

    commit

--- parstagram/AppDelegate.swift
+++ parstagram/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Parse
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "AYG52HJkv6gX6iytfTzJhd8G2hqSBu0Mz4O0kmg4" 
+                    $0.clientKey = "lkU3IxwQO3YfK4EwVFgIsJBFG9tpeASu4wE94Mu6" 
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
---
+++ parstagram/LoginViewController.swift
@@ -0,0 +1,56 @@
+//
+//  LoginViewController.swift
+//  parstagram
+//
+//  Created by Tazkia Afra on 10/23/20.
+//
+
+import UIKit
+import Parse
+
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password)
+        { (user, error) in
+            if user != nil {
+            self.performSegue(withIdentifier: "loginSegue", sender: nil)
+        } else {
+            print("Error: \(error?.localizedDescription)")
+        }
+        }
+    }
+    
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+         
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription))")
+            }
+        
+    
+    
+    
+        }
+    }
+
+    }
